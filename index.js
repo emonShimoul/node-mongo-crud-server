bf7a08a65bf7ad62c607fe82721734c1ad6466a9
emonShimoul <emonshimoul@gmail.com>
2022-06-24 13:56:41 +0600

    added user in the db

--- index.js
+++ index.js
@@ -10,9 +10,17 @@ const port = process.env.PORT || 5000;
 const uri = "mongodb+srv://mydbuser1:5GTBgBhpP8tye52q@cluster0.pabg0.mongodb.net/?retryWrites=true&w=majority";
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 client.connect(err => {
-  const collection = client.db("test").collection("devices");
+  const collection = client.db("foodMaster").collection("users");
   // perform actions on the collection object
-  client.close();
+  console.log('Hitting the database');
+
+  const user = {name: 'Mahiya Mahi', email: "mahi@gmail.com", phone: "01999999999"};
+  collection.insertOne(user)
+  .then(() => {
+    console.log("Insert Success!!");
+  })
+
+//   client.close();
 });
 
 app.get('/', (req, res) => {
@@ -21,4 +29,4 @@ app.get('/', (req, res) => {
 
 app.listen(port, () => {
     console.log('Running Server on port ', port);
-})
+});
