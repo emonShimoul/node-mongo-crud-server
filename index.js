881da1bb544c566072ef00b9315a88f04c5d6446
emonShimoul <emonshimoul@gmail.com>
2022-06-24 14:03:21 +0600

    connect to cloud db and send data to db using async await

--- index.js
+++ index.js
@@ -9,19 +9,24 @@ const port = process.env.PORT || 5000;
 
 const uri = "mongodb+srv://mydbuser1:5GTBgBhpP8tye52q@cluster0.pabg0.mongodb.net/?retryWrites=true&w=majority";
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
-client.connect(err => {
-  const collection = client.db("foodMaster").collection("users");
-  // perform actions on the collection object
-  console.log('Hitting the database');
 
-  const user = {name: 'Opu Bishwas', email: "opu@gmail.com", phone: "01999999888"};
-  collection.insertOne(user)
-  .then(() => {
-    console.log("Insert Success!!");
-  })
-
-//   client.close();
-});
+async function run() {
+    try {
+      await client.connect();
+      const database = client.db("foodMaster");
+      const usersCollection = database.collection("users");
+      // create a document to insert
+      const doc = {
+        name: "SpecialOne",
+        email: "specialone@gmail.com",
+      }
+      const result = await usersCollection.insertOne(doc);
+      console.log(`A document was inserted with the _id: ${result.insertedId}`);
+    } finally {
+      await client.close();
+    }
+  }
+  run().catch(console.dir);
 
 app.get('/', (req, res) => {
     res.send('Running my CRUD Server...');
