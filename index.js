600feea4b5a05bd607a11b479eff9cc61fd041aa
emonShimoul <emonshimoul@gmail.com>
2022-06-25 01:11:11 +0600

    hit the server by clicking the update button

--- index.js
+++ index.js
@@ -29,6 +29,12 @@ async function run() {
         res.send(users);
       })
 
+      app.get('/users/:id', async(req, res) => {
+        const id = req.params.id;
+        console.log('load user with id: ', id);
+        res.send('getting soon');
+      })
+
       // POST API
       app.post('/users', async(req, res) => {
         const newUser = req.body;
