d554526bbcc5c5d7839490338571edaa6663948d
emonShimoul <emonshimoul@gmail.com>
2022-06-25 00:53:34 +0600

    delete an item from the database using delete api

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const { MongoClient, ServerApiVersion } = require('mongodb');
 const cors = require('cors');
+const ObjectId = require('mongodb').ObjectId;
 
 const app = express();
 const port = process.env.PORT || 5000;
@@ -35,7 +36,18 @@ async function run() {
         console.log(`Got new user: `, req.body);
         console.log('added user', result);
         res.json(result);
+      });
+
+      // DELETE API
+      app.delete('/users/:id', async(req, res) => {
+        const id = req.params.id;
+        const query = { _id: ObjectId(id) };
+        const result = await usersCollection.deleteOne(query);
+        console.log('deleting user with id ', result);
+        console.log('deleting user with id ', id);
+        res.json(result)
       })
+
     } finally {
       // await client.close();
     }
